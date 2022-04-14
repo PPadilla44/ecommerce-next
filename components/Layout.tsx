52332cf5337160862a0b41bcb905b4e39cfdce4d
PPadilla44 <Padilla.pablo1998@gmail.com>
2022-04-14 10:59:06 -0700

    add material ui theme

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
-import React, { PropsWithChildren } from 'react'
+import React from 'react'
 import NextLink from "next/link"
-import { AppBar, Toolbar, Typography, Container, Link } from "@material-ui/core"
+import { AppBar, Toolbar, Typography, Container, Link, createTheme, ThemeProvider, CssBaseline } from "@material-ui/core"
 import useStyles from "../utils/styles"
 
 
@@ -13,40 +13,72 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children, title, description }) => {
 
+    const theme = createTheme({
+        typography: {
+            h1: {
+                fontSize: "1.6rem",
+                fontWeight: 400,
+                margin: "1rem 0"
+            },
+            h2: {
+                fontSize: "1.4rem",
+                fontWeight: 400,
+                margin: "1rem 10"
+            },
+            body1: {
+                fontWeight: "normal"
+            }
+        },
+        palette: {
+            type: "light",
+            primary: {
+                main: "#f0c000"
+            },
+            secondary: {
+                main: "#208080"
+            }
+        }
+    })
     const classes = useStyles();
 
     return (
         <div>
+
             <Head>
-                <title>{ title ? `${title} - Next Amazona` : 'Next Amazona' }</title>
-                { description && <meta name="description" content={description}/> }
+                <title>{title ? `${title} - Next Amazona` : 'Next Amazona'}</title>
+                {description && <meta name="description" content={description} />}
             </Head>
-            <AppBar position='static' className={classes.navbar}>
-                <Toolbar>
-                    <NextLink href={"/"} passHref>
-                        <Link>
-                            <Typography className={classes.brand}>amazona</Typography>
-                        </Link>
-                    </NextLink>
-                    <div className={classes.grow}></div>
-                    <div>
-                        <NextLink href={"/cart"} passHref>
-                            <Link>Cart</Link>
+
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <AppBar position='static' className={classes.navbar}>
+                    <Toolbar>
+                        <NextLink href={"/"} passHref>
+                            <Link>
+                                <Typography className={classes.brand}>amazona</Typography>
+                            </Link>
                         </NextLink>
-                        <NextLink href={"/login"} passHref>
-                            <Link>Login</Link>
-                        </NextLink>
-                    </div>
-                </Toolbar>
-            </AppBar>
-            <Container className={classes.main}>
-                {children}
-            </Container>
-            <footer className={classes.footer}>
-                <Typography>
-                    All right reserved. Next Amazona
-                </Typography>
-            </footer>
+                        <div className={classes.grow}></div>
+                        <div>
+                            <NextLink href={"/cart"} passHref>
+                                <Link>Cart</Link>
+                            </NextLink>
+                            <NextLink href={"/login"} passHref>
+                                <Link>Login</Link>
+                            </NextLink>
+                        </div>
+                    </Toolbar>
+                </AppBar>
+                <Container className={classes.main}>
+                    {children}
+                </Container>
+                <footer className={classes.footer}>
+                    <Typography>
+                        All right reserved. Next Amazona
+                    </Typography>
+                </footer>
+            </ThemeProvider>
+
         </div>
     )
 }
